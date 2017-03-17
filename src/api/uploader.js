113219c702619d019044fe000695df5546541cf2
adjhernandez <aaron@manhattanmobile.net>
2017-03-16 17:50:00 -0700

    working upload component.

---
+++ src/api/uploader.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { eventBus } from '../main';
+
+export const uploadToS3 = (file, callback) => {
+  _getSignedRequest(file)
+  .then(_uploadToS3)
+  .then((message) => {
+    callback(null, message);
+  })
+  .catch((err) => {
+    callback(err, null);
+  });
+}
+
+let _getSignedRequest = (file) => {
+  let _file = file;
+  let promise = new Promise((resolve, reject) => {
+    const filename = _file.name;
+    const filetype = _file.type;
+    const url = `http://localhost:4000/v1/api/sign?filename=${filename}&filetype=${filetype}`
+    axios.get(url)
+    .then((response) => {
+      resolve({signedUrl: response.data.data.signedUrl, url: response.data.data.url, file: _file});
+    })
+    .catch((error) => {
+      reject(JSON.stringify(error));
+    });
+  });
+  return promise;
+}
+
+let _uploadToS3 = (payload) => {
+  let _payload = payload;
+  let promise = new Promise((resolve, reject) => {
+    var config = {
+      headers: {
+        'Content-Type': _payload.file.type
+      },
+      onUploadProgress: function(progressEvent) {
+        let percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+        eventBus.$emit('uploadProgress', {percentCompleted: percentCompleted});
+      }
+    };
+    let _this = this;
+    axios.put(_payload.signedUrl, _payload.file, config)
+    .then(function (res) {
+      resolve({message: 'upload success!'});
+    })
+    .catch(function (err) {
+      reject(JSON.stringify(err));
+    });
+  });
+  return promise;
+}
--- src/components/Podcasts.vue
+++ src/components/Podcasts.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Podcasts go here.</h1>
     <h1>Upload this</h1>
+    <h1>{{ message }}</h1>
     <div v-if="!image">
       <h2>Select an image</h2>
       <input id="file" type="file" @change="onFileChange">
@@ -10,79 +11,51 @@
       <img :src="image" />
       <button @click="removeImage">Remove image</button>
     </div>
-
   </div>
 </template>
 
 <script>
 
-import axios from 'axios';
+  import { uploadToS3 } from '../api/uploader';
+  import { eventBus } from '../main';
 
-//upload functions
-function getSignedRequest(file){
-
-  const url = `https://smmapi-dev.herokuapp.com/v1/api/getSignedUrlForImage?objectName=${file.name}&file-type=${file.type}`
-  axios.get(url)
-  .then((response) => {
-    let signed = response.data.signedRequest;
-    uploadToS3(signed, file);
-  })
-  .catch((error) => console.log(error));
-
-};
-
-function uploadToS3(signedUrl, file) {
-
-          var config = {
-            onUploadProgress: function(progressEvent) {
-              var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
-              console.log(percentCompleted);
-            },
-            headers: {
-              'Content-Type': file.type
-            }
-          };
-
-          axios.put(signedUrl, file, config)
-            .then(function (res) {
-              console.log(res.data);
-            })
-            .catch(function (err) {
-              console.log(err);
-            });
-
-};
-
-
-export default {
-  data: function () {
-    return {
-      statusMessage: 'Please select a file',
-      image: ''
-    }
-  },
-  methods: {
-    onFileChange(e) {
-      var files = e.target.files || e.dataTransfer.files;
-      if (!files.length)
-       return;
-      this.createImage(files[0]);
+  export default {
+    data: function () {
+      return {
+        statusMessage: 'Please select a file',
+        image: '',
+        message: ''
+      }
     },
-    createImage(file) {
-      var image = new Image();
-      var reader = new FileReader();
-      var vm = this;
-      reader.onload = (e) => {
-       vm.image = e.target.result;
-       getSignedRequest(file);
-      };
-      reader.readAsDataURL(file);
+    created() {
+      eventBus.$on('uploadProgress', (data) => {
+        console.log("upload progress " + data.percentCompleted + "%");
+      });
     },
-    removeImage: function (e) {
-      this.image = '';
+    methods: {
+      onFileChange(e) {
+        let files = e.target.files || e.dataTransfer.files;
+        if (!files.length) return;
+        this.createImage(files[0]);
+      },
+      createImage(file) {
+        let image = new Image();
+        const reader = new FileReader();
+        let vm = this;
+        reader.onload = (e) => {
+         vm.image = e.target.result;
+         uploadToS3(file, (err, message) => {
+           console.log(err);
+           console.log(message);
+         });
+        };
+        reader.readAsDataURL(file);
+      },
+      removeImage() {
+        this.image = '';
+      }
     }
-  }
-};
+  };
 
 </script>
 
