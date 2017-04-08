e440a3b6d06b467261b01a39c11f6219aeb048c6
adjhernandez <aaron@manhattanmobile.net>
2017-04-07 22:53:04 -0700

    web pack fixes for production

--- src/components/classes/VideoTest.vue
+++ src/components/classes/VideoTest.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-      <video-player :options="videoOptions" @player-state-changed="playerStateChanged" ref="myPlayer"></video-player>
-  <div>
-    <button type="button" @click="changeVideo">Open Logan</button>
-  </div>
-  <div>
-    <button type="button" @click="changeVideo2">Open Wick</button>
-  </div>
+    <video-player :options="videoOptions" @player-state-changed="playerStateChanged" ref="myPlayer"></video-player>
+    <div>
+      <button type="button" @click="changeVideo">Open Logan</button>
     </div>
+    <div>
+      <button type="button" @click="changeVideo2">Open Wick</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,7 +31,7 @@
       }
     },
     methods: {
-      changeVideo(){
+      changeVideo() {
         let _this = this;
         this.videoOptions.source.src = "https://d9iiow8rnlprs.cloudfront.net/logan/758294637291.m3u8";
         setTimeout(() => {
@@ -39,7 +39,7 @@
           _this.player.play();
         }, 100);
       },
-      changeVideo2(){
+      changeVideo2() {
         let _this = this;
         this.videoOptions.source.src = "https://d9iiow8rnlprs.cloudfront.net/johnwick2/encoded-Tue-Jan-2017-04-35-07/encoded-Tue-Jan-2017-04-35-07.m3u8";
         setTimeout(() => {
@@ -48,11 +48,13 @@
         }, 100);
       },
       playerStateChanged(playerCurrentState) {
-      console.log(Math.round((playerCurrentState.currentTime / this.player.duration())*100));
+        console.log(Math.round((playerCurrentState.currentTime / this.player.duration()) * 100));
       }
     }
   }
+
 </script>
 
 <style lang="css">
+
 </style>
