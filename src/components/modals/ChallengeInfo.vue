acb53997e68d69866d8e0afd0fa262fbe5f7d122
aaronhernandez <aaron@manhattanmobile.net>
2017-07-28 11:09:36 -0700

    added showing of challenge info modal when tapping on details

---
+++ src/components/modals/ChallengeInfo.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="modalShowChallengeInfo" class="modal modal--s" :class="{'is--visible': active}">
+        <div class="modal__wrapper modal__wrapper--transition no--pad">
+            <div class="modal__body">
+                <div class="well no--border">
+                    <!-- PANEL BODY -->
+                    <div class="panel__body">
+                        <div class="align--center">
+                            <div>There is some explanation about the challenge detail here</div>
+                            <button @click="handleAction">Ok</button>
+                        </div>
+                    </div>
+                    <!-- /PANEL BODY -->
+                </div>
+            </div>
+        </div>
+        <div class="modal__overlay" @click="close"></div>
+    </div>
+
+</template>
+
+<script>
+    import { User, Class } from '../../api';
+    import { mapGetters } from 'vuex';
+    import { eventBus } from '../../main';
+
+    export default {
+        mounted() {
+
+        },
+        computed: {
+            ...mapGetters([
+                'hasModal', 'activeModal', 'user'
+            ]),
+            active() {
+                if ((this.hasModal) && (this.activeModal == 'challengeinfo')) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        methods: {
+            close() {
+                this.$store.dispatch('updateHasModal', false);
+                this.$store.dispatch('updateActiveModal', '');
+            },
+            handleAction() {
+                this.$store.dispatch('updateHasModal', false);
+                this.$store.dispatch('updateActiveModal', '');
+            },
+        }
+    }
+
+</script>
+<style>
+
+</style>
